c9889df3c6599a3ee99543a50617dc00202b53d4
MateoDedi <mateod@live.com>
2023-09-28 09:33:39 +0200

    added 02

---
+++ Exo_02.js
@@ -0,0 +1,13 @@
+function reverseOdd(str) {
+    const words = str.split(/\s+/);
+    for (let i = 0; i < words.length; i++) {
+        if (words[i].length % 2 === 1) {
+            words[i] = words[i].split('').reverse().join('');
+        }
+    }
+    return words.join(' ');
+}
+
+console.log(reverseOdd("Bananas"));
+console.log(reverseOdd("One two three four"));
+console.log(reverseOdd("Make sure uoy only esrever sdrow of ddo length"));
